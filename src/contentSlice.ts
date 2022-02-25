c460512e845e68898003eeaa5d939db4e6429472
elisey <crowfoody@gmail.com>
2022-02-25 14:46:46 +0300

    setting up redux

---
+++ src/contentSlice.ts
@@ -0,0 +1,43 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+export interface AppState {
+    type: number,
+    category: number,
+    color: number,
+    dark: number,
+    mockup: number
+}
+
+const initialState: AppState = {
+    type: 0,
+    category: 0,
+    color: 0,
+    dark: 0,
+    mockup: 0
+}
+
+export const contentSlice = createSlice({
+    name: 'app',
+    initialState,
+    reducers: {
+        changeType: (state, action: PayloadAction<number>) => {
+            state.type = action.payload;
+        },
+        changeCategory: (state, action: PayloadAction<number>) => {
+            state.category = action.payload;
+        },
+        changeColor: (state, action: PayloadAction<number>) => {
+            state.color = action.payload;
+        },
+        changeDark: (state, action: PayloadAction<number>) => {
+            state.dark = action.payload;
+        },
+        changeMockup: (state, action: PayloadAction<number>) => {
+            state.mockup = action.payload;
+        },
+    },
+})
+
+export const {changeType, changeCategory, changeColor, changeDark, changeMockup} = contentSlice.actions;
+
+export default contentSlice.reducer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {store} from './store';
+import {Provider} from 'react-redux';
+
 import './scss/index.scss';
 import 'normalize.css';
 
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
