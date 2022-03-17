ac424899110087fb8b6854936b562178f19b6a1b
elisey <crowfoody@gmail.com>
2022-03-17 20:22:59 +0300

    testimonial styles

--- src/components/molecules/Testimonial.tsx
+++ src/components/molecules/Testimonial.tsx
@@ -15,12 +15,12 @@ const Testimonial: FC<Props> = (props) => {
         <div className={className}>
             <img src={quotes} alt="quotes icon" className="testimonial__icon" />
             <div className="testimonial__wrapper">
-                <p className="testimonial__text">{props.children}</p>
+                <div className="testimonial__text">{props.children}</div>
                 <div className="testimonial__author-box">
                     <img src={props.photoSrc} alt="testimonial author" className="testimonial__author-photo" />
                     <div className="testimonial__name-box">
-                        <span className="testimonial__name">{props.name}</span>
-                        <span className="testimonial__info">{props.info}</span>
+                        <div className="testimonial__name">{props.name}</div>
+                        <div className="testimonial__info">{props.info}</div>
                     </div>
                 </div>
             </div>
