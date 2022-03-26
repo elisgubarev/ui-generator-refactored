6781ef86b63818905feedb1dbc0c7e11d9332837
elisey <crowfoody@gmail.com>
2022-03-26 21:17:51 +0400

    tooltip css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,19 @@
 import Sidebar from '../components/layouts/Sidebar';
-import Result from '../components/molecules/Result';
-import ResultSection from '../components/organisms/ResultSection';
-import H1Title from '../components/organisms/H1Title';
-import Testimonial from '../components/molecules/Testimonial';
-import photo from '../assets/images/photo.png';
-import Footer from '../components/organisms/Footer';
-import BottomInfoLayout from '../components/layouts/BottomInfoLayout';
 import MainLayout from '../components/layouts/MainLayout';
+import ReactTooltip from 'react-tooltip';
 
 
 function App() {
     return (
-        <div className="dev-app">
+        <>
+            <ReactTooltip
+                place="top"
+                type="dark"
+                effect="solid"
+            />
             <Sidebar />
             <MainLayout />
-        </div>
+        </>
     );
 }
 
--- src/components/organisms/H1Title.tsx
+++ src/components/organisms/H1Title.tsx
@@ -1,9 +1,46 @@
 import { FC } from 'react';
 import { composeClassList } from '../../functions/composeClassList';
 import H1 from '../../components/headings/H1';
+import landing from '../../assets/images/useCaseLanding.png';
+import prototype from '../../assets/images/useCasePrototype.png';
+import presentation from '../../assets/images/useCasePresentation.png';
+import blog from '../../assets/images/useCaseBlog.png';
+import { idText } from 'typescript';
+
+type TipProps = {
+    noTip?: boolean;
+    id: number;
+}
+
+const Tip: FC<TipProps> = (props) => {
+    const image = [landing, prototype, presentation, blog];
+    const { id, noTip } = props;
+    if (noTip) {
+        return (
+            <>
+                {props.children}
+            </>
+        );
+    }
+    return (
+        <>
+            <span
+                data-html={true}
+                data-tip={`<img class='tooltip-custom__picture' src=${image[id]} alt='Use case ${id}'/>`}
+                data-class="tooltip-custom tooltip-custom_image"
+                className="tooltip-custom__use-case"
+                data-place="bottom"
+            >
+                {props.children}
+            </span>
+        </>
+    );
+
+}
 
 type Props = {
     className?: string;
+    noTip?: boolean;
 }
 
 const H1Title: FC<Props> = (props) => {
@@ -13,7 +50,12 @@ const H1Title: FC<Props> = (props) => {
             <div className="h1-title__wrapper">
                 <H1 className="h1-title__h1">{props.children}</H1>
                 <div className="h1-title__subtitle">
-                    Create mockup (fake) UI for your landing page, prototype, presentation, or blog post. Download free PNG files.
+                    Create mockup (fake) UI for your{' '}
+                    <Tip id={0} noTip={props.noTip}>landing page</Tip>,{' '}
+                    <Tip id={1} noTip={props.noTip}>prototype</Tip>,{' '}
+                    <Tip id={2} noTip={props.noTip}>presentation</Tip>, or{' '}
+                    <Tip id={3} noTip={props.noTip}>blog post</Tip>.{' '}
+                    Download free PNG files.
                 </div>
             </div>
         </div>
